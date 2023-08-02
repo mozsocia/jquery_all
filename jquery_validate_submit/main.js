182aaca93090a4720472bc7f031a0193e50d7343
mozsocia <mozsocia@gmail.com>
2023-08-02 11:15:09 +0600

    project folder unpack

---
+++ jquery_validate_submit/main.js
@@ -0,0 +1,71 @@
+$("#basic-form").validate({
+  rules: {
+    name: {
+      required: true,
+      minlength: 3
+    },
+    age: {
+      required: true,
+      number: true,
+      min: 18
+    },
+    email: {
+      required: true,
+      email: true
+    },
+    weight: {
+      required: {
+        depends: function (elem) {
+          return $("#age").val() > 50
+        }
+      },
+      number: true,
+      min: 0
+    }
+  },
+  messages: {
+    name: {
+      minlength: "Name should be at least 3 characters"
+    },
+    age: {
+      required: "Please enter your age",
+      number: "Please enter your age as a numerical value",
+      min: "You must be at least 18 years old"
+    },
+    email: {
+      email: "The email should be in the format: abc@domain.tld"
+    },
+    weight: {
+      required: "People with age over 50 have to enter their weight",
+      number: "Please enter your weight as a numerical value"
+    }
+  },
+
+
+  submitHandler: function (form) {
+    $.ajax({
+      url: form.action,
+      type: form.method,
+      data: $(form).serialize(),
+      success: function (response) {
+        $('#answers').html(response);
+      }
+    });
+  }
+  ,
+
+
+  submitHandler: function (form) {
+    $.ajax({
+      url: 'response.php',
+      type: 'POST',
+      data: $(form).serialize(),
+      success: function (response) {
+        $('#' + form.id + ' .ht-response-data').html(response);
+      }
+    });
+  }
+});
+
+
+
